42bba5ea095f96f678b5a764500ff347e8158e5e
kristoprasetya <75110757+kristoprasetya@users.noreply.github.com>
2020-11-27 21:36:46 +0700

    Add files via upload

---
+++ MID - Kristo Amadeus.cpp
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+struct Node {
+  int a,b;
+  int number;
+  char name[255];
+  int date;
+  int year;
+  char month[255];
+};
+
+Data *createData (char *name, char *month, int date, int number, int year){
+  Data *newData = (Data*)malloc(sizeof(Data));
+  strcpy(newData->name, name);
+  newData->number;
+  newData->date;
+  newData->year;
+  newData->month;
+}
+
+int main(){
+  scanf("%d %d", &a, &b);
+  scanf("&%d &%c &%d - &%c &%c", date, month, year, name name);
+  scanf("&%d &%c &%d - &%c &%c", date, month, year, name name);
+  scanf("&%d &%c &%d - &%c &%c", date, month, year, name name);
+  scanf("&%d &%c &%d - &%c &%c", date, month, year, name name);
+  scanf("&%d &%c &%d - &%c &%c", date, month, year, name name);
+  scanf("&%d &%c &%d - &%c &%c", date, month, year, name name);
+  scanf("&%d &%c &%d - &%c &%c", date, month, year, name name);
+  printf("Need 2 more cute");
+  printf("%d %c %d - %c %c", date monthm year, name, name);
+  printf("%d %c %d - %c %c", date monthm year, name, name);
+}
+
